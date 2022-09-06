d861b3f3c4285f27a0e6a542fe6661790e14f415
Mehmet <mathdenizi@gmail.com>
2022-09-06 16:06:07 +0200

    sen kediye neler yaptin

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="header">
       <img src="../images/trollface.png" className="header--image" />
       <h2 className="header--title">Header component</h2>
-      <h4 className="header--project">React Course - Project 3</h4>
+      <h4 className="header--project">MEME GENERATOR</h4>
     </header>
   );
 }
